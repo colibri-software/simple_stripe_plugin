9a338d59a2a691f270db633eb920c3ee27c80a5b
Charlie Greene <greeneca@gmail.com>
2014-05-07 13:20:42 -0300

    set current site on base controller

--- app/controllers/simple_stripe_plugin/base_controller.rb
+++ app/controllers/simple_stripe_plugin/base_controller.rb
@@ -2,5 +2,24 @@ require'stripe'
 
 module SimpleStripePlugin
   class BaseController < ActionController::Base
+    before_filter :set_current_site
+
+    def current_site
+      Thread.current[:site]
+    end
+
+    private
+
+    def fetch_site
+      if Locomotive.config.multi_sites?
+        @current_site ||= Locomotive::Site.match_domain(request.host).first
+      else
+        @current_site ||= Locomotive::Site.first
+      end
+    end
+
+    def set_current_site
+      Thread.current[:site] = fetch_site
+    end
   end
 end
