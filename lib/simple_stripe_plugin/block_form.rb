08bed8ed8602f41b0cab468e45687990a9a0c169
Charlie Greene <greeneca@gmail.com>
2014-05-22 12:30:32 -0300

    modify submit model option to be a variable instead of a string

--- lib/simple_stripe_plugin/block_form.rb
+++ lib/simple_stripe_plugin/block_form.rb
@@ -12,6 +12,7 @@ module SimpleStripePlugin
     end
 
     def render(context)
+      @options[:submit_model] = context[@options[:submit_model]]
       form_start = ERB.new(File.read(File.join(File.dirname(__FILE__), 'form_start.erb'))).result binding
       fields = render_all(@nodelist, context)
       form_end = ERB.new(File.read(File.join(File.dirname(__FILE__), 'form_end.erb'))).result binding
